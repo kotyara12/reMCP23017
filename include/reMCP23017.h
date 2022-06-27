95ea22625206ab3eec0f5e40cb4482b12bd84116
kotyara12 <kotyara12@yandex.ru>
2022-06-27 10:32:30 +0300

    First commit

---
+++ include/reMCP23017.h
@@ -0,0 +1,97 @@
+/* 
+   EN: Class for working with the MCP23017 port expander
+   RU: Класс для работы с расширителем портов MCP23017
+   --------------------------------------------------------------------------------
+   (с) 2022 Разживин Александр | Razzhivin Alexander
+   kotyara12@yandex.ru | https://kotyara12.ru | tg: @kotyara1971
+*/
+
+#ifndef __RE_MCP23017_H__
+#define __RE_MCP23017_H__
+
+#include <stdint.h>
+#include <esp_err.h>
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "rTypes.h"
+#include "reGpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// GPIO mode
+typedef enum {
+  MCP23017_GPIO_OUTPUT = 0,
+  MCP23017_GPIO_INPUT
+} mcp23017_gpio_mode_t;
+
+// Interrupt outputs (INTA/INTB) mode
+typedef enum {
+  MCP23017_ACTIVE_LOW = 0,   // Low level on interrupt
+  MCP23017_ACTIVE_HIGH,      // High level on interrupt
+  MCP23017_OPEN_DRAIN        // Open drain
+} mcp23017_int_out_mode_t;
+
+// Interrupt inputs mode
+typedef enum {
+  MCP23017_INT_DISABLED = 0, // No interrupt
+  MCP23017_INT_LOW_EDGE,     // Interrupt on low edge
+  MCP23017_INT_HIGH_EDGE,    // Interrupt on high edge
+  MCP23017_INT_ANY_EDGE      // Interrupt on any edge
+} mcp23017_gpio_intr_t;
+
+class reMCP23017 {
+  public:
+    reMCP23017(i2c_port_t numI2C, uint8_t addrI2C, cb_gpio_change_t callback);
+    ~reMCP23017();
+
+    // Configure
+    bool configGet(mcp23017_int_out_mode_t *intOutMode, bool *intOutMirror);
+    bool configSet(mcp23017_int_out_mode_t intOutMode, bool intOutMirror);
+
+    // Inputs / outputs
+    bool portGetMode(uint16_t * value);
+    bool portSetMode(uint16_t value);
+    bool pinGetMode(uint8_t pin, mcp23017_gpio_mode_t *mode);
+    bool pinSetMode(uint8_t pin, mcp23017_gpio_mode_t mode);
+
+    // Internal pullup
+    bool portGetPullup(uint16_t * value);
+    bool portSetPullup(uint16_t value);
+    bool pinGetPullup(uint8_t pin, bool *enable);
+    bool pinSetPullup(uint8_t pin, bool enable);
+
+    // Unbuffered read / write
+    bool portRead(uint16_t * value);
+    bool portWrite(uint16_t value);
+    bool pinRead(uint8_t pin, bool *level);
+    bool pinWrite(uint8_t pin, bool level);
+
+    // Output latch
+    bool portGetLatch(uint16_t * value);
+    bool portSetLatch(uint16_t value);
+    bool pinGetLatch(uint8_t pin, bool *level);
+    bool pinSetLatch(uint8_t pin, bool level);
+
+    // Port interrupts
+    bool portSetInterrupt(uint16_t mask, mcp23017_gpio_intr_t intr);
+
+    // If interrupts are detected, events will be posted to the event loop
+    bool update();
+  private:
+    i2c_port_t _numI2C = 0; 
+    uint8_t _addrI2C = 0;
+    cb_gpio_change_t _callback = nullptr;
+
+    bool read8(uint8_t reg, uint8_t* value);
+    bool write8(uint8_t reg, uint8_t value);
+    bool read16(uint8_t reg, uint16_t* value);
+    bool write16(uint8_t reg, uint16_t value);
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __RE_MCP23017_H__
---
+++ src/reMCP23017.cpp
@@ -0,0 +1,379 @@
+#include "reMCP23017.h"
+#include "reI2C.h"
+#include "rTypes.h"
+#include "rLog.h"
+#include "reEvents.h"
+
+static const char* logTAG = "MCP23017";
+
+#define REG_IODIRA       0x00   // IODIR: I/O DIRECTION REGISTER (ADDR 0x00)
+#define REG_IODIRB       0x01   //        Controls the direction of the data I/O. 1 = Pin is configured as an input. 0 = Pin is configured as an output.
+#define REG_IPOLA        0x02   // IPOL: INPUT POLARITY PORT REGISTER (ADDR 0x01)
+#define REG_IPOLB        0x03   //        If a bit is set, the corresponding GPIO register bit will reflect the inverted value on the pin.
+#define REG_GPINTENA     0x04   // GPINTEN: INTERRUPT-ON-CHANGE PINS (ADDR 0x02)
+#define REG_GPINTENB     0x05   //        General purpose I/O interrupt-on-change bits. 1 = Enables GPIO input pin for interrupt-on-change event
+#define REG_DEFVALA      0x06   // DEFVAL: DEFAULT VALUE REGISTER (ADDR 0x03)
+#define REG_DEFVALB      0x07   //        Sets the compare value for pins configured for interrupt-on-change from defaults. If the associated pin level is the opposite from the register bit, an interrupt occurs
+#define REG_INTCONA      0x08   // INTCON: INTERRUPT-ON-CHANGE CONTROL REGISTER (ADDR 0x04)
+#define REG_INTCONB      0x09   //        Controls how the associated pin value is compared for interrupt-on-change. 1 = Pin value is compared against the associated bit in the DEFVAL register. Pin value is compared against the previous pin value
+#define REG_IOCON        0x0A   // CONFIGURATION REGISTER
+#define REG_GPPUA        0x0C   // GPPU: GPIO PULL-UP RESISTOR REGISTER (ADDR 0x06)
+#define REG_GPPUB        0x0D   //        Controls the weak pull-up resistors on each pin (when configured as an input). 1 = Pull-up enabled. 0 = Pull-up disabled
+#define REG_INTFA        0x0E   // INTF: INTERRUPT FLAG REGISTER (ADDR 0x07)
+#define REG_INTFB        0x0F   //        Reflects the interrupt condition on the port. It reflects the change only if interrupts are enabled per GPINTEN. 1 = Pin caused interrupt. 0 = Interrupt not pending
+#define REG_INTCAPA      0x10   // INTCAP: INTERRUPT CAPTURED VALUE FOR PORT REGISTER (ADDR 0x08)
+#define REG_INTCAPB      0x11   //        Reflects the logic level on the port pins at the time of interrupt due to pin change. 1 = Logic-high. 0 = Logic-low
+#define REG_GPIOA        0x12   // GPIO: GENERAL PURPOSE I/O PORT REGISTER (ADDR 0x09)
+#define REG_GPIOB        0x13   //        Reflects the logic level on the pins. 1 = Logic-high. 0 = Logic-low
+#define REG_OLATA        0x14   // OLAT: OUTPUT LATCH REGISTER 0 (ADDR 0x0A)
+#define REG_OLATB        0x15   //        Reflects the logic level on the output latch. 1 = Logic-high. 0 = Logic-low
+
+#define BIT_IOCON_INTPOL BIT1   // This bit sets the polarity of the INT output pin. 1 = Active-high. 0 = Active-low
+#define BIT_IOCON_ODR    BIT2   // Configures the INT pin as an open-drain output. 1 = Open-drain output (overrides the INTPOL bit.). 0 = Active driver output (INTPOL bit sets the polarity.)
+#define BIT_IOCON_HAEN   BIT3   // Hardware Address Enable bit (MCP23S17 only)
+#define BIT_IOCON_DISSLW BIT4   // Slew Rate control bit for SDA output. 1 = Slew rate disabled. 0 = Slew rate enabled
+#define BIT_IOCON_SEQOP  BIT5   // Sequential Operation mode bit. 1 = Sequential operation disabled, address pointer does not increment. 0 = Sequential operation enabled, address pointer increments
+#define BIT_IOCON_MIRROR BIT6   // INT Pins Mirror bit. 1 = The INT pins are internally connected. 0 = The INT pins are not connected. INTA is associated with PORTA and INTB is associated with PORTB
+#define BIT_IOCON_BANK   BIT7   // Controls how the registers are addressed. 1 = The registers associated with each port are separated into different banks. 0 = The registers are in the same bank (addresses are sequential).
+
+#define MCP23017_TIMEOUT 1000
+
+reMCP23017::reMCP23017(i2c_port_t numI2C, uint8_t addrI2C, cb_gpio_change_t callback)
+{
+  _numI2C = numI2C; 
+  _addrI2C = addrI2C;
+  _callback = callback;
+}
+
+reMCP23017::~reMCP23017()
+{
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------- I2C ---------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::read8(uint8_t reg, uint8_t* value)
+{
+  esp_err_t err = readI2C(_numI2C, _addrI2C, &reg, 1, value, 1, 0, MCP23017_TIMEOUT); 
+  if (err == ESP_OK) {
+    // rlog_d(logTAG, "Read from MCP23017 %d.0x%2x register 0x%2x: 0x%2x", _numI2C, _addrI2C, reg, *value);
+    return true;
+  };
+  rlog_e(logTAG, "Failed to read MCP23017 %d.0x%2x register 0x%2x: %d (%s)", _numI2C, _addrI2C, reg, err, esp_err_to_name(err));
+  return false;
+}
+
+bool reMCP23017::write8(uint8_t reg, uint8_t value)
+{
+  esp_err_t err = writeI2C(_numI2C, _addrI2C, &reg, 1, &value, 1, MCP23017_TIMEOUT);
+  if (err == ESP_OK) {
+    // rlog_d(logTAG, "Write to MCP23017 %d.0x%2x register 0x%2x: 0x%2x", _numI2C, _addrI2C, reg, value);
+    return true;
+  };
+  rlog_e(logTAG, "Failed to write MCP23017 %d.0x%2x register 0x%2x: %d (%s)", _numI2C, _addrI2C, reg, err, esp_err_to_name(err));
+  return false;
+}
+
+bool reMCP23017::read16(uint8_t reg, uint16_t* value)
+{
+  esp_err_t err = readI2C(_numI2C, _addrI2C, &reg, 1, (uint8_t*)value, 2, 0, MCP23017_TIMEOUT); 
+  if (err == ESP_OK) {
+    // rlog_d(logTAG, "Read from MCP23017 %d.0x%2x register 0x%2x: 0x%4x", _numI2C, _addrI2C, reg, *value);
+    return true;
+  };
+  rlog_e(logTAG, "Failed to read MCP23017 %d.0x%2x register 0x%2x: %d (%s)", _numI2C, _addrI2C, reg, err, esp_err_to_name(err));
+  return false;
+}
+
+bool reMCP23017::write16(uint8_t reg, uint16_t value)
+{
+  esp_err_t err = writeI2C(_numI2C, _addrI2C, &reg, 1, (uint8_t*)&value, 2, MCP23017_TIMEOUT);
+  if (err == ESP_OK) {
+    // rlog_d(logTAG, "Write to MCP23017 %d.0x%2x register 0x%2x: 0x%4x", _numI2C, _addrI2C, reg, value);
+    return true;
+  };
+  rlog_e(logTAG, "Failed to write MCP23017 %d.0x%2x register 0x%2x: %d (%s)", _numI2C, _addrI2C, reg, err, esp_err_to_name(err));
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------ Configure ------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::configGet(mcp23017_int_out_mode_t *intOutMode, bool *intOutMirror)
+{
+  uint8_t config;
+  if (read8(REG_IOCON, &config)) {
+    if (config & BIT_IOCON_ODR) {
+      *intOutMode = MCP23017_OPEN_DRAIN;
+    } else {
+      if (config & BIT_IOCON_INTPOL) {
+        *intOutMode = MCP23017_ACTIVE_HIGH;
+      } else {
+        *intOutMode = MCP23017_ACTIVE_LOW;
+      };
+    };
+    *intOutMirror = (config & BIT_IOCON_MIRROR);
+    return true;
+  };
+  return false;
+}
+
+bool reMCP23017::configSet(mcp23017_int_out_mode_t intOutMode, bool intOutMirror)
+{
+  uint8_t config;
+  if (read8(REG_IOCON, &config)) {
+    if (intOutMode == MCP23017_ACTIVE_HIGH) {
+      config &= ~BIT_IOCON_ODR;
+      config |= BIT_IOCON_INTPOL;
+    } else if (intOutMode == MCP23017_ACTIVE_LOW) {
+      config &= ~BIT_IOCON_ODR;
+      config &= ~BIT_IOCON_INTPOL;
+    } else {
+      config |= BIT_IOCON_ODR;
+      config &= ~BIT_IOCON_INTPOL;
+    };
+    intOutMirror ? config |= BIT_IOCON_MIRROR : config &= ~BIT_IOCON_MIRROR;
+    config &= ~BIT_IOCON_BANK;
+    return write8(REG_IOCON, config);
+  };
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------- Inputs / outputs ---------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::portGetMode(uint16_t * value)
+{
+  return read16(REG_IODIRA, value);
+}
+
+bool reMCP23017::portSetMode(uint16_t value)
+{
+  return write16(REG_IODIRA, value);
+}
+
+bool reMCP23017::pinGetMode(uint8_t pin, mcp23017_gpio_mode_t *mode)
+{
+  uint16_t buf;
+  if (read16(REG_IODIRA, &buf)) {
+    if ((buf & (1 << pin)) > 0) {
+      *mode = MCP23017_GPIO_INPUT;
+    } else {
+      *mode = MCP23017_GPIO_OUTPUT;
+    };
+    return true;
+  };
+  return false;
+}
+
+bool reMCP23017::pinSetMode(uint8_t pin, mcp23017_gpio_mode_t mode)
+{
+  uint16_t buf;
+  if (read16(REG_IODIRA, &buf)) {
+    (mode == MCP23017_GPIO_INPUT) ? buf |= (1 << pin) : buf &= ~(1 << pin);
+    return write16(REG_IODIRA, buf);
+  };
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------- Internal pullup ---------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::portGetPullup(uint16_t * value)
+{
+  return read16(REG_GPPUA, value);
+}
+
+bool reMCP23017::portSetPullup(uint16_t value)
+{
+  return write16(REG_GPPUA, value);
+}
+
+bool reMCP23017::pinGetPullup(uint8_t pin, bool *enable)
+{
+  uint16_t buf;
+  if (read16(REG_GPPUA, &buf)) {
+    *enable = (buf & (1 << pin)) > 0;
+    return true;
+  };
+  return false;
+}
+
+bool reMCP23017::pinSetPullup(uint8_t pin, bool enable)
+{
+  uint16_t buf;
+  if (read16(REG_GPPUA, &buf)) {
+    enable ? buf |= (1 << pin) : buf &= ~(1 << pin);
+    return write16(REG_GPPUA, buf);
+  };
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------- Unbuffered read / write -----------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::portRead(uint16_t * value)
+{
+  return read16(REG_GPIOA, value);
+}
+
+bool reMCP23017::portWrite(uint16_t value)
+{
+  return write16(REG_GPIOA, value);
+}
+
+bool reMCP23017::pinRead(uint8_t pin, bool *level)
+{
+  uint16_t buf;
+  if (read16(REG_GPIOA, &buf)) {
+    *level = (buf & (1 << pin)) > 0;
+    return true;
+  };
+  return false;
+}
+
+bool reMCP23017::pinWrite(uint8_t pin, bool level)
+{
+  uint16_t buf;
+  if (read16(REG_GPIOA, &buf)) {
+    level ? buf |= (1 << pin) : buf &= ~(1 << pin);
+    return write16(REG_GPIOA, buf);
+  };
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------- Latch outputs ----------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * OUTPUT LATCH REGISTER (OLAT)
+ * The OLAT register provides access to the output latches. 
+ * A read from this register results in a read of the OLAT and not the port itself. 
+ * A write to this register modifies the output latches that modifies the pins configured as outputs.
+ **/
+
+bool reMCP23017::portGetLatch(uint16_t * value)
+{
+  return read16(REG_OLATA, value);
+}
+
+bool reMCP23017::portSetLatch(uint16_t value)
+{
+  return write16(REG_OLATA, value);
+}
+
+bool reMCP23017::pinGetLatch(uint8_t pin, bool *level)
+{
+  uint16_t buf;
+  if (read16(REG_OLATA, &buf)) {
+    *level = (buf & (1 << pin)) > 0;
+    return true;
+  };
+  return false;
+}
+
+bool reMCP23017::pinSetLatch(uint8_t pin, bool level)
+{
+  uint16_t buf;
+  if (read16(REG_OLATA, &buf)) {
+    level ? buf |= (1 << pin) : buf &= ~(1 << pin);
+    return write16(REG_OLATA, buf);
+  };
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------- Port interrupts ---------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::portSetInterrupt(uint16_t mask, mcp23017_gpio_intr_t intr)
+{
+  // GPINTEN: INTERRUPT-ON-CHANGE PINS (ADDR 0x02)
+  // General purpose I/O interrupt-on-change bits. 
+  // 1 = Enables GPIO input pin for interrupt-on-change event
+  // 0 = Disables GPIO input pin for interrupt-on-change event
+  uint16_t int_en;
+  if (read16(REG_GPINTENA, &int_en)) {
+    if (intr == MCP23017_INT_DISABLED) {
+      // Disable interrupts
+      int_en &= ~mask;
+    } else {
+      // Enable interrupts
+      int_en |= mask;
+
+      // INTCON: INTERRUPT-ON-CHANGE CONTROL REGISTER (ADDR 0x04)
+      // Controls how the associated pin value is compared for interrupt-on-change
+      // 1 = Pin value is compared against the associated bit in the DEFVAL register
+      // 0 = Pin value is compared against the previous pin value.
+      uint16_t int_con;
+      if (read16(REG_INTCONA, &int_con)) {
+        if (intr == MCP23017_INT_ANY_EDGE) {
+          // Pin value is compared against the previous pin value
+          int_con &= ~mask;
+        } else {
+          // Pin value is compared against the associated bit in the DEFVAL register
+          int_con |= mask;
+
+          // DEFVAL: DEFAULT VALUE REGISTER (ADDR 0x03)
+          // Sets the compare value for pins configured for interrupt-on-change from defaults
+          // If the associated pin level is the opposite from the register bit, an interrupt occurs. 
+          uint16_t int_def;
+          if (read16(REG_DEFVALA, &int_def)) {
+            if (intr == MCP23017_INT_LOW_EDGE) {
+              int_def |= mask;
+            } else {
+              int_def &= ~mask;
+            };
+            if (!write16(REG_DEFVALA, int_def)) return false;
+          };
+        };
+        if (!write16(REG_INTCONA, int_con)) return false;
+      };
+    };
+    return write16(REG_GPINTENA, int_en);
+  };
+  return false;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------ Refresh of interrupts ------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+
+bool reMCP23017::update()
+{
+  uint16_t flags;
+  if (read16(REG_INTFA, &flags)) {
+    if (flags != 0) {
+      // At least one PIN caused an interrupt
+      uint16_t pins;
+      if (!read16(REG_GPIOA, &pins)) {
+        return false;
+      };
+      
+      // Prepare data for events
+      gpio_data_t data;
+      data.bus = (uint8_t)_numI2C + 1;
+      data.address = _addrI2C;
+
+      // Send events only for pins that have changed
+      for (uint8_t i = 0; i < 16; i++) {
+        if ((flags & (1 << i)) > 0) {
+          data.pin = i;
+          data.value = (uint8_t)((pins & (1 << i)) > 0);
+          eventLoopPost(RE_GPIO_EVENTS, RE_GPIO_CHANGE, &data, sizeof(data), portMAX_DELAY);
+          if (_callback) {
+            _callback((void*)this, data, 0);
+          };
+        };
+      };
+    };
+    return true;
+  };
+  return false;
+}
